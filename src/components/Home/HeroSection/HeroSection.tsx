72b8c26550dda3ac640c99111e114125e0bbfe78
Khandaker Nabiul <na.ninad60@gmail.com>
2023-07-03 04:13:47 +0600

    Complete footer and navbar

--- src/components/Home/HeroSection/HeroSection.tsx
+++ src/components/Home/HeroSection/HeroSection.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 
 import style from './heroSection.module.scss';
 
-const { heroSection } = style;
+const { heroSection, small } = style;
 
 const HeroSection = () => {
   return (
     <div className={heroSection}>
       <h1>Welcome to Grameen Kalyan</h1>
+      <h2>Welcome to Grameen Kalyan</h2>
+      <h3>Welcome to Grameen Kalyan</h3>
+      <h4>Welcome to Grameen Kalyan</h4>
+      <h5>Welcome to Grameen Kalyan</h5>
+      <h6>Welcome to Grameen Kalyan</h6>
+      <h6 className={small}>Welcome to Grameen Kalyan</h6>
     </div>
   );
 };
--- src/shared/components/Navbar/Navbar.tsx
+++ src/shared/components/Navbar/Navbar.tsx
@@ -1,23 +1,44 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 
 import style from './navbar.module.scss';
 
 const {
-  navbar, logoContainer, logo, list, item, subItem, dropdownItem, dropdownList
+  navbar,
+  logoContainer,
+  logo,
+  list,
+  item,
+  subItem,
+  dropdownItem,
+  dropdownList,
+  itemText,
+  menuBtn,
+  openList,
+  active,
+  bar
 } = style;
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
   return (
     <div className={navbar}>
       <div className={logoContainer}>
         <div className={logo}></div>
       </div>
-      <ul className={list}>
+      <ul className={toggle ? `${list} ${openList}` : `${list}`}>
         <li className={item}>home</li>
         <li className={item}>about us</li>
         <li className={item}>services</li>
         <li className={item}>healthcare</li>
-        <li className={dropdownItem}>people
+        <li className={dropdownItem}>
+          <span className={itemText}>people</span>
           <ul className={dropdownList}>
             <li className={subItem}>corporate</li>
             <li className={subItem}>health force</li>
@@ -25,13 +46,17 @@ const Navbar = () => {
           </ul>
         </li>
         <li className={item}>stories</li>
-        <li className={dropdownItem}>gallery
+        <li className={dropdownItem}>
+          <span className={itemText}>gallery</span>
           <ul className={dropdownList}>
             <li className={subItem}>photo album</li>
           </ul>
         </li>
         <li className={item}>locator</li>
       </ul>
+      <button className={toggle ? `${menuBtn} ${active}` : `${menuBtn}`} onClick={handleToggle}>
+        <span className={bar}></span>
+      </button>
     </div>
   );
 };
