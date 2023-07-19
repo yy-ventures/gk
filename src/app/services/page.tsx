82186e4cd111c06b7550037229bd764f1e991bfa
Khandaker Nabiul <na.ninad60@gmail.com>
2023-07-19 16:17:55 +0600

    Create service page

---
+++ src/app/services/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { HeroSection } from '@/shared/components';
+import { AreaWeServe } from '@/components/Services';
+
+const Services = () => {
+  return (
+    <>
+      <HeroSection/>
+      <AreaWeServe/>
+    </>
+  );
+};
+
+export default Services;
