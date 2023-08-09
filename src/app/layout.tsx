c234f1be047e5f7d8d02b8037c9dc5c9690b5c83
Khandaker Nabiul <na.ninad60@gmail.com>
2023-08-10 01:49:23 +0600

    Board member page design and api integrate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className} suppressHydrationWarning={true}>
         <Navbar badgeImage={badgeImage}/>
         {children}
         <SidebarSticky/>
