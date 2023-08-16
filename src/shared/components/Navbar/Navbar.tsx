6b583e4b3df0f180a5e7b9272c222da4b50002b9
Khandaker Nabiul <na.ninad60@gmail.com>
2023-08-16 11:38:04 +0600

    Add navbar navigation active

--- src/shared/components/Navbar/Navbar.tsx
+++ src/shared/components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 
@@ -30,7 +30,9 @@ const {
   img,
   spaceBox,
   link,
-  subItemNoBorder
+  subItemNoBorder,
+  activeItem,
+  activeSubItem
 } = style;
 
 interface INavbarProps {
@@ -40,6 +42,16 @@ interface INavbarProps {
 const Navbar = ({ badgeImage }: INavbarProps) => {
   const pathName = usePathname();
   const [toggle, setToggle] = useState(false);
+  const [isHomePage, setIsHomePage] = useState(false);
+  const [isAboutPage, setIsAboutPage] = useState(false);
+  const [isServicesPage, setIsServicesPage] = useState(false);
+  const [isHealthcarePage, setIsHealthcarePage] = useState(false);
+  const [isCorporatePage, setIsCorporatePage] = useState(false);
+  const [isHealthForcePage, setIsHealthForcePage] = useState(false);
+  const [isJoinUsPage, setIsJoinUsPage] = useState(false);
+  const [isStoriesPage, setIsStoriesPage] = useState(false);
+  const [isPhotoGalleryPage, setIsPhotoGalleryPage] = useState(false);
+  const [isLocatorPage, setIsLocatorPage] = useState(false);
 
   const handleToggle = () => {
     setToggle(!toggle);
@@ -48,8 +60,188 @@ const Navbar = ({ badgeImage }: INavbarProps) => {
     setToggle(false);
   };
 
-  const isHomePage = pathName === '/';
   const badge = IMAGE_BASE_URL + badgeImage;
+  // const isHomePage = pathName === '/';
+
+  const handleActiveHomePage = (page: string) => {
+    switch (page) {
+    case 'home':
+      setIsHomePage(true);
+      setIsAboutPage(false);
+      setIsServicesPage(false);
+      setIsHealthcarePage(false);
+      setIsCorporatePage(false);
+      setIsHealthForcePage(false);
+      setIsJoinUsPage(false);
+      setIsStoriesPage(false);
+      setIsPhotoGalleryPage(false);
+      setIsLocatorPage(false);
+      break;
+    case 'about':
+      setIsHomePage(false);
+      setIsAboutPage(true);
+      setIsServicesPage(false);
+      setIsHealthcarePage(false);
+      setIsCorporatePage(false);
+      setIsHealthForcePage(false);
+      setIsJoinUsPage(false);
+      setIsStoriesPage(false);
+      setIsPhotoGalleryPage(false);
+      setIsLocatorPage(false);
+      break;
+    case 'services':
+      setIsHomePage(false);
+      setIsAboutPage(false);
+      setIsServicesPage(true);
+      setIsHealthcarePage(false);
+      setIsCorporatePage(false);
+      setIsHealthForcePage(false);
+      setIsJoinUsPage(false);
+      setIsStoriesPage(false);
+      setIsPhotoGalleryPage(false);
+      setIsLocatorPage(false);
+      break;
+    case 'healthcare':
+      setIsHomePage(false);
+      setIsAboutPage(false);
+      setIsServicesPage(false);
+      setIsHealthcarePage(true);
+      setIsCorporatePage(false);
+      setIsHealthForcePage(false);
+      setIsJoinUsPage(false);
+      setIsStoriesPage(false);
+      setIsPhotoGalleryPage(false);
+      setIsLocatorPage(false);
+      break;
+    case 'corporate':
+      setIsHomePage(false);
+      setIsAboutPage(false);
+      setIsServicesPage(false);
+      setIsHealthcarePage(false);
+      setIsCorporatePage(true);
+      setIsHealthForcePage(false);
+      setIsJoinUsPage(false);
+      setIsStoriesPage(false);
+      setIsPhotoGalleryPage(false);
+      setIsLocatorPage(false);
+      break;
+    case 'health-force':
+      setIsHomePage(false);
+      setIsAboutPage(false);
+      setIsServicesPage(false);
+      setIsHealthcarePage(false);
+      setIsCorporatePage(false);
+      setIsHealthForcePage(true);
+      setIsJoinUsPage(false);
+      setIsStoriesPage(false);
+      setIsPhotoGalleryPage(false);
+      setIsLocatorPage(false);
+      break;
+    case 'join-us':
+      setIsHomePage(false);
+      setIsAboutPage(false);
+      setIsServicesPage(false);
+      setIsHealthcarePage(false);
+      setIsCorporatePage(false);
+      setIsHealthForcePage(false);
+      setIsJoinUsPage(true);
+      setIsStoriesPage(false);
+      setIsPhotoGalleryPage(false);
+      setIsLocatorPage(false);
+      break;
+    case 'stories':
+      setIsHomePage(false);
+      setIsAboutPage(false);
+      setIsServicesPage(false);
+      setIsHealthcarePage(false);
+      setIsCorporatePage(false);
+      setIsHealthForcePage(false);
+      setIsJoinUsPage(false);
+      setIsStoriesPage(true);
+      setIsPhotoGalleryPage(false);
+      setIsLocatorPage(false);
+      break;
+    case 'photo-gallery':
+      setIsHomePage(false);
+      setIsAboutPage(false);
+      setIsServicesPage(false);
+      setIsHealthcarePage(false);
+      setIsCorporatePage(false);
+      setIsHealthForcePage(false);
+      setIsJoinUsPage(false);
+      setIsStoriesPage(false);
+      setIsPhotoGalleryPage(true);
+      setIsLocatorPage(false);
+      break;
+    case 'locator':
+      setIsHomePage(false);
+      setIsAboutPage(false);
+      setIsServicesPage(false);
+      setIsHealthcarePage(false);
+      setIsCorporatePage(false);
+      setIsHealthForcePage(false);
+      setIsJoinUsPage(false);
+      setIsStoriesPage(false);
+      setIsPhotoGalleryPage(false);
+      setIsLocatorPage(true);
+      break;
+
+    default:
+      break;
+    }
+  };
+
+  const handleActivePage = (path: string) => {
+    switch (path) {
+    case '/':
+      handleActiveHomePage('home');
+      setToggle(false);
+      break;
+    case '/about':
+      handleActiveHomePage('about');
+      setToggle(false);
+      break;
+    case '/services':
+      handleActiveHomePage('services');
+      setToggle(false);
+      break;
+    case '/healthcare':
+      handleActiveHomePage('healthcare');
+      setToggle(false);
+      break;
+    case '/corporate':
+      handleActiveHomePage('corporate');
+      setToggle(false);
+      break;
+    case '/health-force':
+      handleActiveHomePage('health-force');
+      setToggle(false);
+      break;
+    case '/join-us':
+      handleActiveHomePage('join-us');
+      setToggle(false);
+      break;
+    case '/stories':
+      handleActiveHomePage('stories');
+      setToggle(false);
+      break;
+    case '/photo-gallery':
+      handleActiveHomePage('photo-gallery');
+      setToggle(false);
+      break;
+    case '/locator':
+      handleActiveHomePage('locator');
+      setToggle(false);
+      break;
+
+    default:
+      break;
+    }
+  };
+
+  useEffect(() => {
+    setIsHomePage(true);
+  }, []);
 
   return (
     <div className={navbar}>
@@ -59,45 +251,45 @@ const Navbar = ({ badgeImage }: INavbarProps) => {
         </Link>
       </div>
       <ul className={toggle ? `${list} ${openList}` : `${list}`}>
-        <Link className={link} href='/' onClick={closeNavbar}>
-          <li className={item}>home</li>
+        <Link className={link} href='/' onClick={() => handleActivePage('/')}>
+          <li className={isHomePage ? `${item} ${activeItem}` : `${item}`}>home</li>
         </Link>
-        <Link className={link} href='/about' onClick={closeNavbar}>
-          <li className={item}>about us</li>
+        <Link className={link} href='/about' onClick={() => handleActivePage('/about')}>
+          <li className={isAboutPage ? `${item} ${activeItem}` : `${item}`}>about us</li>
         </Link>
-        <Link className={link} href='/services' onClick={closeNavbar}>
-          <li className={item}>services</li>
+        <Link className={link} href='/services' onClick={() => handleActivePage('/services')}>
+          <li className={isServicesPage ? `${item} ${activeItem}` : `${item}`}>services</li>
         </Link>
-        <Link className={link} href='/healthcare' onClick={closeNavbar}>
-          <li className={item}>healthcare</li>
+        <Link className={link} href='/healthcare' onClick={() => handleActivePage('/healthcare')}>
+          <li className={isHealthcarePage ? `${item} ${activeItem}` : `${item}`}>healthcare</li>
         </Link>
         <li className={dropdownItem}>
           <span className={itemText}>people <BiSolidDownArrow/></span>
           <ul className={dropdownList}>
-            <Link className={link} href='/corporate' onClick={closeNavbar}>
-              <li className={subItem}>corporate</li>
+            <Link className={link} href='/corporate' onClick={() => handleActivePage('/corporate')}>
+              <li className={isCorporatePage ? `${subItem} ${activeSubItem}` : `${subItem}`}>corporate</li>
             </Link>
-            <Link className={link} href='/health-force' onClick={closeNavbar}>
-              <li className={subItem}>health force</li>
+            <Link className={link} href='/health-force' onClick={() => handleActivePage('/health-force')}>
+              <li className={isHealthForcePage ? `${subItem} ${activeSubItem}` : `${subItem}`}>health force</li>
             </Link>
-            <Link className={link} href='/join-us' onClick={closeNavbar}>
-              <li className={`${subItem} ${subItemNoBorder}`}>join us</li>
+            <Link className={link} href='/join-us' onClick={() => handleActivePage('/join-us')}>
+              <li className={isJoinUsPage ? `${subItem} ${subItemNoBorder} ${activeSubItem}` : `${subItem} ${subItemNoBorder}`}>join us</li>
             </Link>
           </ul>
         </li>
-        <Link className={link} href='/stories' onClick={closeNavbar}>
-          <li className={item}>stories</li>
+        <Link className={link} href='/stories' onClick={() => handleActivePage('/stories')}>
+          <li className={isStoriesPage ? `${item} ${activeItem}` : `${item}`}>stories</li>
         </Link>
         <li className={dropdownItem}>
           <span className={itemText}>gallery <BiSolidDownArrow/></span>
           <ul className={dropdownList}>
-            <Link className={link} href='/photo-gallery'>
-              <li className={`${subItem} ${subItemNoBorder}`}>photo album</li>
+            <Link className={link} href='/photo-gallery' onClick={() => handleActivePage('/photo-gallery')}>
+              <li className={isPhotoGalleryPage ? `${subItem} ${subItemNoBorder} ${activeSubItem}` : `${subItem} ${subItemNoBorder}`}>photo album</li>
             </Link>
           </ul>
         </li>
-        <Link className={link} href='/locator' onClick={closeNavbar}>
-          <li className={item}>locator</li>
+        <Link className={link} href='/locator' onClick={() => handleActivePage('/locator')}>
+          <li className={isLocatorPage ? `${item} ${activeItem}` : `${item}`}>locator</li>
         </Link>
         {
           !isHomePage
