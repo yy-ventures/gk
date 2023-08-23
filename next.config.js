3c08e905ac573baec81aaf8e334137001960d170
Khandaker Nabiul <na.ninad60@gmail.com>
2023-08-23 11:52:08 +0600

    Update deploy config

--- next.config.js
+++ next.config.js
@@ -3,19 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   optimizeFonts: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'admin.grameenkalyan.com'
-      }
-    ],
     domains: ['admin.grameenkalyan.com', 'res.cloudinary.com']
   }
-  // output: 'export',
-  // env: {
-  //   NEXT_PUBLIC_BASE_URL: 'https://api.grameenkalyan.com/api',
-  //   NEXT_PUBLIC_IMAGE_URL: 'https://admin.grameenkalyan.com'
-  // }
 };
 
 module.exports = nextConfig;
